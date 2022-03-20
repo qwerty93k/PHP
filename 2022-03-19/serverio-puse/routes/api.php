f2327a59fb69d3b92afd0bfdb5635b1141537618
Mindaugas <qwerty93k@gmail.com>
2022-03-20 21:26:07 +0200

    reset

--- 2022-03-19/serverio-puse/routes/api.php
+++ 2022-03-19/serverio-puse/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,5 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::apiResource('images', ImageController::class);
